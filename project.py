73fa11eedcdb90f958b56dce1413352c02ecc7df
segev-harpaz <56492572+segev-harpaz@users.noreply.github.com>
2020-06-09 05:41:19 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ database = database_connection()
 def http_header(packet):
     http_packet = str(packet)
     if 'segevharpaz1.pythonanywhere.com' in http_packet and 'times' in http_packet:
-        print('nigga')
+        print('yes')
         return GET_print(packet)
 
 
@@ -25,6 +25,7 @@ def GET_print(packet1):
         columns.append(data[i].split('=')[0])
     del x[3]
     del columns[3]
+    print(x[3])
     database.connect()
     x.insert(3, str(packet1[0][Ether].src))
     columns.insert(3, 'mac')
@@ -34,11 +35,7 @@ def GET_print(packet1):
         columns = columns + a
         b = [True,True,True,True]
         x = x + b
-        print(x)
-        print('x')
         database.insert('project_database', columns, x)
-        print(x[3])
-        print(x[0])
         database.insert(x[4][:-1], ['name'], [x[0]])
     database.close_db()
 
@@ -52,8 +49,8 @@ def sniff1():
         for row in table_data:
             if row[3] == packets[0][Ether].dst:
                 print(2)
-                column_name = ['connected_wifi', 'connected_time', 'on_time', 'mac']
-                values = [True, database.on_time(row[0]), database.late(row[0]), packets[0][Ether].dst]
+                column_name = ['connected_wifi', 'mac']
+                values = [True, packets[0][Ether].dst]
                 database.update('project_database', column_name, values, True)
         database.close_db()
 
@@ -66,11 +63,13 @@ def sniff2():
 
 def dudu(q):
     global database
+    print('chage true')
     if not q:
+        print('end change')
         return create_q()
     database.connect()
-    column = ['connected_wifi', 'connected_signin', 'connected_time']
-    values = (False, False, False)
+    column = ['connected_wifi', 'connected_signin', 'connected_time', 'on_time']
+    values = (False, False, False, False)
     database.update('project_database', column, values, False)
     data = q.pop(0)
     database.close_db()
@@ -79,40 +78,45 @@ def dudu(q):
 
 def arrive_late(q):
     global database
-    print(2)
+    print('change data')
     if not q:
-        print(1)
+        print('end data')
         return create_q()
     database.connect()
     table_data = database.get_data('project_database')
+    table_data1 = database.get_data('project_database')
     today = date.today()
     for row in table_data:
-        if 'a' + str(today)[6:].replace('-', '') == database.times(row[7][:-1]):
-            if bool(row[4]) & bool(row[5]) and bool(row[6]):
-                if bool(row[8]):
-                    columns = ['a' + str(today)[6:].replace('-', ''), 'name']
-                    values = (0, row[0])
-                    database.update(row[7][:-1], columns, values, True)
+        for i in database.table_column(row[7][:-1]):
+            if ('a' + str(today)[6:].replace('-', '') == i):
+                if bool(row[4]) & bool(row[5]) and bool(row[6]):
+                    if bool(row[8]):
+                        columns = ['a' + str(today)[6:].replace('-', ''), 'name']
+                        values = (0, row[0])
+                        database.update(row[7][:-1], columns, values, True)
+                    else:
+                        columns = ['a' + str(today)[6:].replace('-', ''), 'name']
+                        values = (1, row[0])
+                        database.update(row[7][:-1], columns, values, True)
                 else:
                     columns = ['a' + str(today)[6:].replace('-', ''), 'name']
-                    values = (1, row[0])
+                    values = (2, row[0])
                     database.update(row[7][:-1], columns, values, True)
-            else:
-                columns = ['a' + str(today)[6:].replace('-', ''), 'name']
-                values = (2, row[0])
-                database.update(row[7][:-1], columns, values, True)
+    print(q)
     data = q.pop(0)
+    print(data)
     database.close_db()
     Timer(data, arrive_late, [q]).start()
 
 
 def create_q():
-    q = [timedelta(hours=2).total_seconds(), timedelta(hours=2).total_seconds(),
-         timedelta(hours=2).total_seconds()]
+    q = [timedelta(seconds=180).total_seconds(), timedelta(seconds=180).total_seconds(),
+         timedelta(seconds=180).total_seconds(),timedelta(seconds=180).total_seconds(), timedelta(seconds=180).total_seconds(),
+         timedelta(seconds=180).total_seconds()]
     now = datetime.now().strftime('%H:%M:%S')
     now = datetime.strptime(now, '%H:%M:%S')
-    reset_connection = datetime.strptime('08:20:00', "%H:%M:%S")
-    late_check = datetime.strptime('08:15:00', "%H:%M:%S")
+    reset_connection = datetime.strptime('5:23:00', "%H:%M:%S")
+    late_check = datetime.strptime('05:22:00', "%H:%M:%S")
     delay = (reset_connection - now).total_seconds()
     delay1 = (late_check - now).total_seconds()
     if delay < 0:
